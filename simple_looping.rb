3f71a731bf85e270628af3af525137af08df2f1e
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-03 22:24:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,7 +15,6 @@ def times_iterator(number_of_times)
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
-
 def while_iterator(number_of_times)
    counter = 0
 while counter < 7

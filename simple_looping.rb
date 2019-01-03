feae14d67df83790dc085fbf3a9a563d6ff3b6fa
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-03 22:44:54 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,10 +30,9 @@ def until_iterator(number_of_times)
 end
 end
 
-def for_iterator(x)
+def for_iterator(number_of_times)
+  number_of_times = 1..7
   for number in number_of_times
   puts = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times = 1...7
-end
 end
 end
